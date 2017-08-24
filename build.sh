c89a4ce3531292c5edb437842ff2c69027750dd0
gaeolus <gaeolus@gmail.com>
2017-08-24 20:27:10 +0800

    remove mysql

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t myoj/oj_web_server -f dockerfiles/oj_web_server/Dockerfile  .
 docker build -t myoj/judger -f dockerfiles/judger/Dockerfile  .
-docker build -t myoj/mysql -f dockerfiles/mysql/Dockerfile  .
+
